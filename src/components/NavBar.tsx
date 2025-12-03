d641d7bbd71d1670ddf5c0017a322ff1533d6cee
Abaljerind <abaljerinda@gmail.com>
2025-12-03 22:17:50 +0700

    Fix(NavBar): Fix UI for right side of NavBar
    Re-adjust styles for mobile and desktop width
    Feat UI for user that already login but as comment

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,4 @@
-import { FaRegUser } from "react-icons/fa";
 import Logo from "../assets/logo-light-mode.png";
-import { FiShoppingCart } from "react-icons/fi";
 
 const NavBar = () => {
   return (
@@ -17,7 +15,7 @@ const NavBar = () => {
 
           {/* Desktop NavItem */}
           <div className="mt-1.5 hidden md:block">
-            <ul className="flex items-center justify-evenly gap-4">
+            <ul className="flex items-center justify-evenly gap-8">
               <li className="hover:text-primary">Home</li>
               <li className="hover:text-primary">Products</li>
               <li className="hover:text-primary">Order History</li>
@@ -38,17 +36,23 @@ const NavBar = () => {
 
         {/* Logo + NavItem */}
 
-        {/* Login/User Icon & Cart Icon */}
-        <div className="mt-1.5 flex items-center gap-1.5 md:gap-4">
-          <FaRegUser className="size-5 md:size-6" />
-          <div className="relative p-2.5">
-            <FiShoppingCart className="size-5 md:size-6" />
+        {/* Sign In & Sign Up || Icon Cart & Sign Out */}
+        <div className="mt-1.5 flex items-center gap-4 md:gap-8">
+          <button className="bg-primary text-background rounded-full px-3 py-2 text-sm shadow-md md:px-4 md:text-base">
+            Sign In
+          </button>
+
+          {/* Cart Icon & Sign Out Icon ### TUNGGU FITUR LOGIN ### */}
+          {/* <div className="relative p-2.5">
+            <FiShoppingCart className="size-6 md:size-7" />
             <div className="bg-primary absolute top-0.5 -right-1 flex size-5 items-center justify-center rounded-full md:top-0 md:-right-2 md:size-6">
               <span className="text-xs font-bold text-white md:text-sm">3</span>
             </div>
           </div>
+          <VscSignOut className="size-6 md:size-7" /> */}
+          {/* ./ Cart Icon & Sign Out Icon */}
         </div>
-        {/* ./ Login/User Icon & Cart Icon */}
+        {/* ./ Sign In & Sign Up || Icon Cart & Sign Out */}
       </nav>
       {/* Desktop NavBar */}
     </>
