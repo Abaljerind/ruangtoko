843a9e274cc1eaf41050f69ef626a63c64bba115
Abaljerind <abaljerinda@gmail.com>
2025-12-09 14:34:05 +0700

    Feat(Copyrights): create copyrights component

---
+++ src/components/Copyrights.tsx
@@ -0,0 +1,9 @@
+const Copyrights = () => {
+  return (
+    <p className="pt-4 pb-[70px] text-center text-sm text-gray-500 md:pb-4">
+      &copy; 2025 RuangToko. All Rights Reserved.
+    </p>
+  );
+};
+
+export default Copyrights;
