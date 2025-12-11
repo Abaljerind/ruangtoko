2210f9fbb5e52c0e0a638cb0c39f65f4e1c29fe6
Abaljerind <abaljerinda@gmail.com>
2025-12-11 15:14:35 +0700

    Feat(Stars): create Stars component to display icon star

---
+++ src/components/Stars.tsx
@@ -0,0 +1,15 @@
+import { CiStar } from "react-icons/ci";
+
+const Stars = () => {
+  return (
+    <div className="flex items-center">
+      <CiStar className="size-5 text-yellow-500" />
+      <CiStar className="size-5 text-yellow-500" />
+      <CiStar className="size-5 text-yellow-500" />
+      <CiStar className="size-5 text-yellow-500" />
+      <CiStar className="size-5 text-yellow-500" />
+    </div>
+  );
+};
+
+export default Stars;
