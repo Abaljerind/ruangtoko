5bdd0f6b8c53f5585c2ab620edcd7d0ecdff8db0
Abaljerind <abaljerinda@gmail.com>
2025-12-12 22:08:03 +0700

    Fix(ProductCard): fix style for some elements
    Fix image src and add alt
    Adjust style for mobile, tablet and desktop width

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,17 +7,28 @@ type Props = {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <div className="mx-auto space-y-2 rounded-xl bg-white text-sm xl:text-base">
-      <img src={product.image} alt="" className="w-full rounded-t-lg" />
-      <div className="space-y-2.5 p-4">
-        <p className="font-medium capitalize">{product.title}</p>
-        <div className="flex items-center gap-1.5">
-          <Stars /> {product.rating}
+    <div className="mx-auto w-full max-w-[350px] space-y-2.5 rounded-xl bg-white text-sm xl:text-base">
+      <div className="flex h-40 items-center justify-center">
+        <img
+          src={product.images[0]}
+          alt={product.title}
+          className="h-full object-contain"
+        />
+      </div>
+
+      <div className="p-4">
+        <p className="min-h-10 font-medium capitalize md:mb-1.5 xl:mb-0">
+          {product.title}
+        </p>
+        <div className="space-y-2">
+          <div className="flex items-center gap-1.5">
+            <Stars /> {product.rating}
+          </div>
+          <p className="font-semibold text-gray-700">{product.price}</p>
+          <button className="bg-primary w-full cursor-pointer rounded-lg py-2 text-center text-white hover:bg-blue-700">
+            Add to Cart
+          </button>
         </div>
-        <p className="font-semibold text-gray-700">{product.price}</p>
-        <button className="bg-primary w-full cursor-pointer rounded-lg py-2 text-center text-white hover:bg-blue-700">
-          Add to Cart
-        </button>
       </div>
     </div>
   );
