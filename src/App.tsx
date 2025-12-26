b88e66d366bb845097ce305140bdb2a19c20bdb1
Abaljerind <abaljerinda@gmail.com>
2025-12-26 19:50:33 +0700

    Refactor(App): invoke page ErrorPage as component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ProductCard from "./components/ProductCard";
 import { RiShieldCheckLine, RiTruckLine } from "react-icons/ri";
 import { MdSupportAgent } from "react-icons/md";
 import { useEffect, useState } from "react";
-import errorImage from "./assets/error-img.svg";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   const [isSignUp, setIsSignUp] = useState<string>("");
@@ -50,23 +50,7 @@ function App() {
     setIsSignUp("");
   };
 
-  if (isError) {
-    return (
-      <div className="flex h-screen flex-col items-center justify-center gap-4 px-4 text-center">
-        <img
-          src={errorImage}
-          alt="Error 404 Not Found Illustration"
-          className="w-4/5 object-cover md:w-1/2 lg:w-1/3"
-        />
-        <h2 className="text-3xl font-bold lg:text-4xl">
-          Oops, something went wrong.
-        </h2>
-        <p className="text-sm font-medium lg:text-lg">
-          An unexpected error occurred. We're looking into it.
-        </p>
-      </div>
-    );
-  }
+  if (isError) return <ErrorPage />;
 
   return (
     <>
