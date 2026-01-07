859ef8bad7a2eea8cb250196a8e2129be1325656
Abaljerind <abaljerinda@gmail.com>
2026-01-07 16:36:23 +0700

    Feat(navbar): add modal box and change tag <a> to <Link>
    Import illustration from assets
    Import Link and useState
    Create useState for show and hide modal box pop up
    Create function handleUserIconClick to change useState value
    Change tag <a> to <Link> for all nav-items
    Add conditional rendering using useState to show modal box
    Create modal box as alert

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,17 @@
 import { FiShoppingCart } from "react-icons/fi";
 import Logo from "../assets/logo-light-mode.png";
+import programmer from "../assets/tired-programmer.svg";
 import { FaRegUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isClick, setIsClick] = useState<boolean>(false);
+
+  const handleUserIconClick = () => {
+    setIsClick(!isClick);
+  };
+
   return (
     <>
       {/* Desktop NavBar */}
@@ -25,13 +34,13 @@ const NavBar = () => {
           <div className="mt-1.5 hidden md:block">
             <ul className="flex items-center justify-evenly gap-8">
               <li className="hover:text-secondary font-medium">
-                <a href="#">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li className="hover:text-secondary font-medium">
-                <a href="#">Products</a>
+                <Link to="/products">Products</Link>
               </li>
               <li className="hover:text-secondary font-medium">
-                <a href="#">Order History</a>
+                <Link to="/order">Order History</Link>
               </li>
             </ul>
           </div>
@@ -41,9 +50,15 @@ const NavBar = () => {
         {/* Mobile - NavItem | Bottom Section */}
         <div className="relative md:hidden">
           <ul className="bg-primary fixed right-1/2 bottom-0 flex w-full translate-x-1/2 items-center justify-evenly gap-4 border-t border-gray-400/40 py-4 font-medium text-white md:border-0 md:border-none">
-            <li>Home</li>
-            <li>Products</li>
-            <li>Order History</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/order">Order History</Link>
+            </li>
           </ul>
         </div>
         {/* ./ Mobile - NavItem | Bottom Section */}
@@ -53,18 +68,50 @@ const NavBar = () => {
         {/* Icon Cart & Icon User */}
         <div className="mt-1.5 flex items-center gap-4 md:gap-8">
           <div className="relative p-2.5">
-            <a href="#">
+            <Link to="/cart">
               <FiShoppingCart className="size-6 md:size-7" />
-            </a>
+            </Link>
             <div className="bg-primary absolute top-0.5 -right-1 flex size-5 items-center justify-center rounded-full md:top-0 md:-right-2 md:size-6">
               <span className="text-xs font-bold text-white md:text-sm">3</span>
             </div>
           </div>
-          <FaRegUser className="size-6 cursor-pointer md:size-7" />
+          <FaRegUser
+            className="size-6 cursor-pointer md:size-7"
+            onClick={handleUserIconClick}
+          />
         </div>
         {/* ./ Icon Cart & Icon User */}
       </nav>
       {/* Desktop NavBar */}
+
+      {/* modal box for account page */}
+      {isClick && (
+        <div className="fixed inset-0 z-40 bg-black/60">
+          <div className="absolute top-24 left-1/2 flex w-4/5 max-w-sm -translate-x-1/2 flex-col items-center justify-center gap-3 rounded-lg bg-white p-4 text-center shadow-md md:top-1/6 md:w-2/5">
+            <img
+              src={programmer}
+              alt="tired programmer illustration"
+              className="w-3/4"
+            />
+            <div className="space-y-3">
+              <h2 className="font-poppins text-lg font-medium underline">
+                Feature Not Available
+              </h2>
+              <p className="text-sm text-gray-600">
+                User accounts are not supported at the moment. Please check back
+                later for updates.
+              </p>
+              <button
+                className="bg-primary w-full cursor-pointer rounded-lg py-2 text-center text-white hover:bg-blue-700"
+                onClick={handleUserIconClick}
+              >
+                Understood
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {/* ./ modal box for account page */}
     </>
   );
 };
