1742a302274a8b11842be68a7f25344fe0c77995
Abaljerind <abaljerinda@gmail.com>
2026-01-08 22:38:07 +0700

    Fix(homepageLoader): add return type using ProductResponse

--- apis/homepageLoader.tsx
+++ apis/homepageLoader.tsx
@@ -1,4 +1,6 @@
-export const homepageLoader = async () => {
+import type { ProductResponse } from "../src/types/product-interface";
+
+export const homepageLoader = async (): Promise<ProductResponse> => {
   const response = await fetch("https://dummyjson.com/products");
 
   // check if fetch success or not
