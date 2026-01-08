cf702079fb9af0155c4df4022a7afdbc0f46a273
Abaljerind <abaljerinda@gmail.com>
2026-01-08 22:16:08 +0700

    Feat(index.tsx): add ErrorPage & loader
    Rename loader as homeLoader

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,15 +4,20 @@ import Homepage from "../pages/Homepage";
 import ProductListPage from "../pages/ProductListPage";
 import OrderHistoryPage from "../pages/OrderHistoryPage";
 import CartPage from "../pages/CartPage";
+import ErrorPage from "../pages/ErrorPage";
+
+import { homepageLoader as homeLoader } from "../../apis/homepageLoader";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <Homepage />,
+        loader: homeLoader,
       },
       {
         path: "/products",
