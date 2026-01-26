57d1cb534f4d15c70b6657af1028f901426138fd
Abaljerind <abaljerinda@gmail.com>
2026-01-26 21:45:07 +0700

    Feat(Button): add another variants
    Change style for all variants in variants variable

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,20 @@
-type Variant = "primary" | "secondary" | "card" | "clear";
+type Variant = "primary" | "secondary" | "card" | "cardIcon" | "clear";
 
 type Props = {
-  text: string;
+  text: string | React.ReactNode;
   variant?: Variant;
 };
 
 const Button = ({ text, variant = "primary" }: Props) => {
   const baseStyle: string =
-    "py-2.5 px-4 cursor-pointer rounded-lg text-center transition font-semibold text-sm lg:rounded-xl";
+    "cursor-pointer group rounded-lg text-center transition font-semibold text-sm shadow-md lg:rounded-xl";
 
   const variants: Record<Variant, string> = {
-    primary: "text-primary bg-white hover:bg-white/90",
-    secondary: "bg-blue-600 text-white",
-    card: "bg-primary w-full text-white hover:bg-primary/90",
-    clear: "bg-gray-300 w-full text-black",
+    primary: "py-2.5 px-4 text-primary bg-white hover:bg-white/90",
+    secondary: "py-2.5 px-4 bg-blue-600 text-white",
+    card: "py-2.5 px-4 bg-primary text-white hover:bg-primary/90",
+    cardIcon: "p-2 bg-blue-200 text-blue-600 hover:bg-primary/90",
+    clear: "py-2.5 px-4 bg-gray-300 w-full text-black",
   };
 
   return (
