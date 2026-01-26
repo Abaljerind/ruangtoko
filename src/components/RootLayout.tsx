ab5b33af1f2edef6efb7ccf748aa2025783803ea
Abaljerind <abaljerinda@gmail.com>
2026-01-26 16:38:04 +0700

    Feat(RootLayout): add Footer component

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 const RootLayout = () => {
   return (
@@ -8,6 +9,8 @@ const RootLayout = () => {
       <main>
         <Outlet />
       </main>
+
+      <Footer />
     </>
   );
 };
