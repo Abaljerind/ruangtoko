ba9de7d508decf0d408229be1b6f50766885fcd1
Abaljerind <abaljerinda@gmail.com>
2026-02-10 15:30:51 +0700

    Style: add hover state and delete font color

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -318,7 +318,7 @@ const ProductListPage = () => {
         {visibleCount < sortedProducts.length && (
           <button
             onClick={handleLoadMore}
-            className="mt-6 cursor-pointer rounded-lg bg-gray-300 px-4 py-2 font-semibold text-slate-600"
+            className="mt-6 cursor-pointer rounded-lg bg-gray-300 px-4 py-2 font-semibold duration-300 hover:shadow-lg"
           >
             Load more
           </button>
