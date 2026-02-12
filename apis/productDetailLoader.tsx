9b5b9efec4e9b142ba8d5753a7430ee034b37ef8
Abaljerind <abaljerinda@gmail.com>
2026-02-12 16:13:47 +0700

    Feat(productDetailLoader): add new fetch for category product
    Create interface ProductDetailData
    Change Promise value to ProductDetailData
    Change variable name from res to productRes
    Change variable name from data to product
    Create categoryRes to fetch data based on it's category
    Add conditional statement for categoryRes
    Create variable recommended
    Return product and recommended

--- apis/productDetailLoader.tsx
+++ apis/productDetailLoader.tsx
@@ -1,20 +1,46 @@
 import type { LoaderFunctionArgs } from "react-router-dom";
 import type { ProductType } from "../src/types/product-interface";
 
+export interface ProductDetailData {
+  product: ProductType;
+  recommended: ProductType[];
+}
+
 export const productDetailLoader = async ({
   params,
-}: LoaderFunctionArgs): Promise<ProductType> => {
+}: LoaderFunctionArgs): Promise<ProductDetailData> => {
   const id = Number(params.id);
 
   if (isNaN(id)) {
     throw new Response("Invalid product ID", { status: 400 });
   }
 
-  const res = await fetch(`https://dummyjson.com/products/${id}`);
+  const productRes = await fetch(`https://dummyjson.com/products/${id}`);
 
-  if (!res.ok) {
-    throw new Response("Failed to fetch product", { status: res.status });
+  if (!productRes.ok) {
+    throw new Response("Failed to fetch product", {
+      status: productRes.status,
+    });
   }
-  const data: ProductType = await res.json();
-  return data;
+  const product: ProductType = await productRes.json();
+
+  // fetch product berdasarkan category
+  const categoryRes = await fetch(
+    `https://dummyjson.com/products/category/${product.category}`,
+  );
+
+  if (!categoryRes.ok) {
+    throw new Response("Failed to fetch recommended products", {
+      status: categoryRes.status,
+    });
+  }
+
+  const categoryData = await categoryRes.json();
+
+  // filter product yang id nya sama dengan product yang sedang dibuka agar tidak masuk di rekomendasi
+  const recommended: ProductType[] = categoryData.products.filter(
+    (p: ProductType) => p.id !== product.id,
+  );
+
+  return { product, recommended };
 };
