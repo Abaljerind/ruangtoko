1d45bf865757b562259bdbd03471982a463f9d5c
Abaljerind <abaljerinda@gmail.com>
2026-02-12 12:46:48 +0700

    Style(ProductDetailsPage): add style for breakpoint md and lg
    Add ScrollRestoration from react-router-dom

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, ScrollRestoration, useLoaderData } from "react-router-dom";
 import { productDetailLoader } from "../../apis/productDetailLoader";
 import { PiStarFill } from "react-icons/pi";
 import Button from "../components/Button";
@@ -11,6 +11,7 @@ const ProductDetailsPage = () => {
 
   return (
     <section className="">
+      <ScrollRestoration />
       {/* simple breadcrumb */}
       <nav className="mb-4 text-sm text-gray-500">
         <Link to={"/"}>Home</Link> / <Link to={"/products"}>All Product</Link> /{" "}
@@ -21,56 +22,58 @@ const ProductDetailsPage = () => {
       {/* ./ simple breadcrumb */}
 
       {/* product */}
-      <div className="space-y-4 py-8">
+      <div className="mx-auto max-w-5xl space-y-4 py-8 md:flex md:items-center md:gap-4 lg:justify-between lg:gap-10">
         {/* gambar product */}
-        <div className="">
+        <div className="aspect-square w-96 shrink-0 overflow-hidden">
           <img
             src={product.images[0]}
             alt={product.title}
-            className="aspect-auto object-cover object-center"
+            className="size-full object-cover object-center"
           />
         </div>
         {/* ./ gambar product */}
 
         {/* data product */}
-        <h1 className="text-2xl font-bold lg:text-4xl">{product.title}</h1>
+        <div className="space-y-4">
+          <h1 className="text-2xl font-bold lg:text-4xl">{product.title}</h1>
 
-        <div className="flex items-center gap-2 text-sm">
-          <PiStarFill className="size-5 text-yellow-500" />(
-          {product.reviews.length} reviews)
-        </div>
+          <div className="flex items-center gap-2 text-sm">
+            <PiStarFill className="size-5 text-yellow-500" />(
+            {product.reviews.length} reviews)
+          </div>
 
-        <p className="text-xl font-bold lg:text-2xl">${product.price}</p>
-        <p className="text-sm text-balance text-gray-500">
-          {product.description}
-        </p>
+          <p className="text-xl font-bold lg:text-2xl">${product.price}</p>
+          <p className="text-sm text-balance text-gray-500">
+            {product.description}
+          </p>
 
-        <div className="space-y-1">
-          <h3 className="font-bold">Specifications</h3>
-          <ul className="space-y-1 px-4">
-            <li className="list-disc text-sm text-gray-500">
-              Brand: {product.brand}
-            </li>
-            <li className="list-disc text-sm text-gray-500">
-              SKU: {product.sku}
-            </li>
-            <li className="list-disc text-sm text-gray-500">
-              Weight: {product.weight} g
-            </li>
-            <li className="list-disc text-sm text-gray-500">
-              Dimensions: {product.dimensions.height}" H x{" "}
-              {product.dimensions.width}" W x {product.dimensions.depth}" D
-            </li>
-          </ul>
-        </div>
+          <div className="space-y-1">
+            <h3 className="font-bold">Specifications</h3>
+            <ul className="space-y-1 px-4">
+              <li className="list-disc text-sm text-gray-500">
+                Brand: {!product.brand ? "Not Available" : product.brand}
+              </li>
+              <li className="list-disc text-sm text-gray-500">
+                SKU: {product.sku}
+              </li>
+              <li className="list-disc text-sm text-gray-500">
+                Weight: {product.weight} g
+              </li>
+              <li className="list-disc text-sm text-gray-500">
+                Dimensions: {product.dimensions.height}" H x{" "}
+                {product.dimensions.width}" W x {product.dimensions.depth}" D
+              </li>
+            </ul>
+          </div>
 
-        <div className="flex flex-col items-center gap-2">
-          <Button
-            text={"Add to Cart"}
-            icon={<FiShoppingCart className="size-6 md:size-7" />}
-            variant="cardWithIcon"
-          />
-          <Button text={"Buy Now"} variant="clear" />
+          <div className="flex flex-col items-center gap-2 md:flex-row lg:gap-4">
+            <Button
+              text={"Add to Cart"}
+              icon={<FiShoppingCart className="size-6 md:size-7" />}
+              variant="cardWithIcon"
+            />
+            <Button text={"Buy Now"} variant="clear" />
+          </div>
         </div>
         {/* ./ data product */}
       </div>
